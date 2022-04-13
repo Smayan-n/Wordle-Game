3bd7bebfc552888cf4e4ee142e01a45f72d073fb
Smayan Nirantare <smayannirantare5@gmail.com>
2022-04-13 16:21:52 -0700

    commit

--- Wordle.py
+++ Wordle.py
@@ -30,7 +30,6 @@ class Window(QMainWindow):
         self.keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
                  "U", "V", "W", "X", "Y", "Z"]
 
-
         #first call to init game
         self.importWords()
         self.initGame()
